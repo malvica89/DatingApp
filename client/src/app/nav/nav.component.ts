70dce3b60c945bcea71838199c463e519181feba
Malvika Sharma <MSharma2@edc.ca>
2022-08-12 13:11:43 -0400

    done

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { user } from '../_model/user';
 import { AccountService } from '../_services/account.service';
@@ -13,7 +14,7 @@ export class NavComponent implements OnInit {
   model : any = {}
   //loggedIn : boolean;
  //currentUser$:Observable<user>;
-  constructor(public accountService:AccountService) { }
+  constructor(public accountService:AccountService, private router:Router) { }
 
   ngOnInit(): void {
     //this.currentUser$ = this.accountService.currentUser$;
@@ -24,6 +25,7 @@ export class NavComponent implements OnInit {
     {
       this.accountService.login(this.model).subscribe(response=>{
         console.log(response);
+        this.router.navigateByUrl('/members');
         //this.loggedIn = true;
       }, error=>
       {
@@ -34,6 +36,7 @@ export class NavComponent implements OnInit {
 
     logout(){
       this.accountService.logout();
+      this.router.navigateByUrl('/');
      // this.loggedIn = false;
     }
 
